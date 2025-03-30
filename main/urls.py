b47aca4739dcceb6309e6718557c27bf92676dbc
Mercycermy <mercycermy5@gmail.com>
2025-03-30 07:23:39 -1200

    adding views for admin and edit profile for user

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('change-password/', ChangePassword.as_view()),
     path('logout/', LogoutView.as_view()),
+    path('edit-profile/', EditProfile.as_view()),
+    path('feedback/', FeedbackView.as_view()),
+    path('notifications/', NotificationView.as_view()),
+    path('admin-dashboard/', AdminDashboard.as_view()),
+    path('admin/edit-user/<int:user_id>/', EditUser.as_view()),
+    path('admin/delete-user/<int:user_id>/', DeleteUser.as_view()),
+    path('admin/reply-feedback/<int:feedback_id>/', ReplyFeedback.as_view()),
+    path('admin/export-users/', ExportUsers.as_view()),
+    path('search/', SearchView.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -5,6 +5,11 @@ from django.contrib import messages
 
 from django.views import View
 from django.contrib.auth.models import User, auth
+from .models import Feedback, Notification, DeletedUser
+from django.utils import timezone
+import openpyxl
+from django.http import HttpResponse
+from django.db.models import Q
 
 
 class IndexView(View):
@@ -131,3 +136,109 @@ class ChangePassword(View):
                                  "Old Password doesn't match.")
 
         return redirect('/change-password')
+#Edit Profile
+class EditProfile(View):
+    def get(self, request):
+        return render(request, "edit_profile.html")
+
+    def post(self, request):
+        user = request.user
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.email = request.POST.get('email')
+        user.save()
+        messages.success(request, "Profile updated successfully!")
+        return redirect('/edit-profile')
+
+#Send Feedback
+class FeedbackView(View):
+    def get(self, request):
+        feedbacks = Feedback.objects.filter(user=request.user)
+        return render(request, "feedback.html", {"feedbacks": feedbacks})
+
+    def post(self, request):
+        msg = request.POST.get('message')
+        Feedback.objects.create(user=request.user, message=msg)
+        messages.success(request, "Feedback sent.")
+        return redirect('/feedback')
+
+# View Notifications
+class NotificationView(View):
+    def get(self, request):
+        notifs = Notification.objects.filter(user=request.user)
+        return render(request, "notifications.html", {"notifications": notifs})
+
+# Admin Dashboard
+class AdminDashboard(View):
+    def get(self, request):
+        if not request.user.is_superuser:
+            return redirect('/')
+        users = User.objects.all()
+        feedbacks = Feedback.objects.all()
+        deleted = DeletedUser.objects.all()
+        return render(request, "admin_dashboard.html", {
+            "users": users,
+            "feedbacks": feedbacks,
+            "deleted": deleted
+        })
+
+# Admin Edit/Delete Users
+class EditUser(View):
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        return render(request, "edit_user.html", {"user": user})
+
+    def post(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.email = request.POST.get('email')
+        user.save()
+        messages.success(request, "User updated.")
+        return redirect('/admin-dashboard')
+
+class DeleteUser(View):
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        DeletedUser.objects.create(username=user.username, email=user.email)
+        user.delete()
+        messages.success(request, "User deleted.")
+        return redirect('/admin-dashboard')
+
+# Admin Reply to Feedback
+class ReplyFeedback(View):
+    def post(self, request, feedback_id):
+        feedback = Feedback.objects.get(id=feedback_id)
+        feedback.reply = request.POST.get('reply')
+        feedback.save()
+        messages.success(request, "Replied to feedback.")
+        return redirect('/admin-dashboard')
+
+#Export to Excel
+class ExportUsers(View):
+    def get(self, request):
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.append(["ID", "Username", "Email", "First Name", "Last Name"])
+        for user in User.objects.all():
+            ws.append([user.id, user.username, user.email, user.first_name, user.last_name])
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=users.xlsx'
+        wb.save(response)
+        return response
+
+# Search Users or Feedback
+class SearchView(View):
+    def get(self, request):
+        query = request.GET.get('q')
+        users = User.objects.filter(
+            Q(username__icontains=query) | Q(email__icontains=query)
+        )
+        feedbacks = Feedback.objects.filter(
+            Q(message__icontains=query)
+        )
+        return render(request, "search.html", {
+            "users": users,
+            "feedbacks": feedbacks,
+            "query": query
+        })
